ed1f83f6b4778ca0f3f5580ce6e7dd4d74c1bfea
nicapos <annikaposadas22@gmail.com>
2023-04-14 19:16:54 +0800

    style: update layout for image comparison

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import filter
 import streamlit as st
 
+st.set_page_config(layout="wide")
 
-image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+image_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+btn_slot = st.empty()   # Create empty slot for conditional button
 
-if image is not None:
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+# Display original image and segmented image only after source image upload
+if image_upload:
+    col_left, col_right = st.columns(2)
 
-    if(st.button('Image Segmentation')):
-        newImage = filter.image_segmentation(image)
-    
-        if(newImage is not None):
-            st.image(newImage, caption='Segmented Image.', use_column_width=True)
-    
+    image = None
+    show_segmented = False
+
+    with col_left:
+        st.subheader("Original image")
+
+        if image_upload is not None:
+            image = image_upload
+
+            st.image(image_upload, use_column_width=True)
+
+            if btn_slot.button('Apply Image Segmentation'):
+                show_segmented = True
+                
+        
+    with col_right:
+        st.subheader("Segmented Image")
+
+        if show_segmented:
+            new_image = filter.image_segmentation(image)
+            
+            if new_image is not None:
+                st.image(new_image, use_column_width=True)
